354be8ae5703a51561225e0a755bd675f303546c
Jan_Podavka <jan.podavka@tul.cz>
2022-03-29 00:52:54 +0200

    edit test codecover

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def test_tests(number_one, number_two):
     """
         testing workflow
     """
-    if number_one > number_two: # test
+    if number_one > number_two:
         return 5
     return 0
 
