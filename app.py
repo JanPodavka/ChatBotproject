e23448b67c6e2d11d7718a0d7bd0d240887239b2
HonzaP <jan.podavka@tul.cz>
2022-04-28 22:19:25 +0200

    HTML time done

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-"""
-    testing module
-"""
+# imports
+import locale
+from datetime import datetime
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 
-def test_tests(number_one, number_two):
-    """
-        testing workflow
-    """
-    if number_one > number_two:
-        return 5
-    return 0
+# define app routes
+@app.route("/")
+def test():
+    return render_template("home.html")
+
+
+
+@app.route("/get")
+def get_bot_response():
+    question = request.args.get('msg')
+    answer = get_answer(question)
+    return str(answer)
+
+def get_answer(question):
+    if question.lower() == "Jaký je čas?".lower():
+        now = datetime.now()
+        return now.strftime("%H:%M:%S")
+
+    else:
+        return "nerozumím"
+
+
+if __name__ == "__main__":
+    app.run()
