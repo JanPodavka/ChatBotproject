64c484a8b12fa0a9c7d8c1104023721be7c0903b
Anastazie Rosova <anastazie.rosova@tul.cz>
2022-05-02 00:17:34 +0200

    add questions and testing

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # imports
 import locale
+import nltk
+import unidecode
+
 from datetime import datetime
 
 from flask import Flask, render_template, request
@@ -30,19 +33,16 @@ def get_bot_response():
 
 
 def get_answer(question):
-    if question.lower() == "Jaký je čas?".lower():
+    norm_question = unidecode.unidecode(question.lower())
+    if nltk.edit_distance(norm_question, "jaky je cas?") < 2:
         now = datetime.now()
         return now.strftime("%H:%M:%S")
-
+    elif nltk.edit_distance(norm_question, "jaky je kurz?") < 2:
+        pass
+    elif nltk.edit_distance(norm_question, "jak se jmenujes?") < 2:
+        return "Jmenuji se Chatbot"
     else:
         return "nerozumím"
-
-
-def fake(a, b):
-    if a == b:
-        return a
-    else:
-        return b
 
 
 if __name__ == "__main__":
